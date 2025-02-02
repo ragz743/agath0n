48e8b55eb84e0e89061763f00bfc001d551dc85b
Neal Wang <nealwang.sh@protonmail.com>
2025-02-01 21:08:23 -0800

    feat: part 1 complete

--- processing/main.py
+++ processing/main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import glob
 import os
-from scipy.spatial import cKDTree
 
 DATA_PATH = "/scratch/project/hackathon/data/SnowpackPredictionChallenge/input_data/"
 
@@ -54,87 +53,25 @@ if __name__ == "__main__":
     swe_df = swe_df.merge(si_df, on=["latitude", "longitude"], how="outer")
 
     print("processing meteorological data...")
+    m_cols = []
     m_dfs = []
-    for path in mdata_paths:
-        si_df = pd.read_csv(path)
-        si_df = si_df.rename(
-            columns={
-                "lat": "latitude",
-                "lon": "longitude",
-                "variable_value": path_to_column_name(path)[16:].lower(),
-            }
+    for i, path in enumerate(mdata_paths):
+        colname = path_to_column_name(path)[16:].lower()
+        print(f"processing {colname} data... ({i + 1}/{len(mdata_paths)})")
+        df = pd.read_csv(path)
+        df = df.rename(
+            columns={"lat": "latitude", "lon": "longitude", "variable_value": colname}
         )
-        m_dfs.append(si_df)
+        m_cols.append(colname)
+        m_dfs.append(df)
     m_df = pd.concat(m_dfs, ignore_index=True)
 
-    print("mapping coordinates...")
-    swe_coords = swe_df[["latitude", "longitude"]].drop_duplicates().to_numpy()
-    m_coords = m_df[["latitude", "longitude"]].drop_duplicates().to_numpy()
+    print("performing mean imputation...")
+    for i, col in enumerate(m_cols):
+        print(f"imputing {col}... ({i + 1}/{len(m_cols)})")
+        mean = m_df[col].mean()
+        m_df[col] = m_df[col].fillna(mean)
 
-    tree = cKDTree(m_coords)
-    _, nearest_idx = tree.query(swe_coords)
-    matched = m_df.iloc[nearest_idx].reset_index(drop=True)
-    coord_mapping = dict(
-        zip(
-            [tuple(coord) for coord in swe_coords],
-            [tuple(coord) for coord in matched[["latitude", "longitude"]].to_numpy()],
-        )
-    )
-
-    def transform_coords(row, mapping):
-        return mapping.get(
-            (row["latitude"], row["longitude"]), (row["latitude"], row["longitude"])
-        )
-
-    print("transforming...")
-    swe_df[["latitude", "longitude"]] = swe_df.apply(
-        transform_coords, axis=1, mapping=coord_mapping, result_type="expand"
-    )
-
-    print("merging...")
-    res = pd.merge(m_df, swe_df, on=["date", "latitude", "longitude"], how="left")
-    res.to_csv("data1.csv", index=False)
-
-    """
-    # locate nearest coords for swe values
-    tree = cKDTree(m_df[["latitude", "longitude"]].to_numpy())
-    _, nearest_idx = tree.query(swe_df[["latitude", "longitude"]].to_numpy())
-    matched = m_df.iloc[nearest_idx].reset_index(drop=True)
-    res_df = swe_df.copy()
-    res_df["matched_latitude"] = matched["latitude"]
-    res_df["matched_longitude"] = matched["longitude"]
-    res_df = res_df.merge(
-        m_df,
-        left_on=["date, matched_latitude, matched_longitude"],
-        right_on=["date", "latitude", "longitude"],
-        how="left",
-    )
-    res_df = res_df.drop(columns=["latitude", "longitude"])
-    res_df = res_df.rename(
-        columns={
-            "matched_latitude": "latitude",
-            "matched_longitude": "longitude",
-        }
-    )
-
-    res_df.to_csv("data1.csv", index=False)
-    """
-
-"""
-# https://www.geeksforgeeks.org/working-with-missing-data-in-pandas/
-# found no missing values
-def find_empty_cells(df):
-    return df.isnull().sum()
-
-
-# found no missing dates
-def find_missing_dates(df):
-    df = df.set_index("date")
-    df.index = pd.to_datetime(df.index)
-    return pd.date_range(start="1990-01-01", end="2019-12-31").difference(df.index)
-
-
-# found duplicate dates; realised this is expected
-def find_duplicate_dates(df):
-    return [i for i, v in df["date"].value_counts().iteritems() if v > 1]
-"""
+    print("saving data...")
+    swe_df.to_hdf("swe_data.h5", key="stage", mode="w")
+    m_df.to_hdf("m_data.h5", key="stage", mode="w")
